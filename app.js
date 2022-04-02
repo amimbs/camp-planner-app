80c9b55febb396c69dce2f4304a589735b0c3baa
amimbs <andymimbs91@gmail.com>
2022-04-02 13:02:19 -0400

    Initialized the server and database

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// App Setup
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//static acts on the first instance of index within the public folder
+app.use(express.static('./public'));
+
+// this communicates with our database
+const models = require('./models');
+
+// the server and port
+app.listen(8080, function () {
+    console.log('The app is now listening on port 8080...');
+});
