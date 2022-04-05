337daf572d6c288914f5caf2c0cf277ccae2b907
amimbs <andymimbs91@gmail.com>
2022-04-05 14:29:01 -0400

    added place holder templates for signin and sign up working on getting the methods for those pages to work

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.urlencoded({ extended: false }));
 //Template Engine Config
 app.set('view engine', 'ejs');
 
+//static acts on the first instance of index within the public folder
+app.use(express.static('./public'));
+
+// this communicates with our database
+const models = require('./models');
+
+//Here are the methods
 app.get('/', (req, res) => {
     // The render method takes the name of the HTML
     // page to be rendered as input
@@ -17,11 +24,24 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-//static acts on the first instance of index within the public folder
-app.use(express.static('./public'));
+app.get('/sign-in', (req, res) => {
+    res.render('signIn');
+});
 
-// this communicates with our database
-const models = require('./models');
+app.get('/sign-up', (req, res) => {
+    res.render('signUp');
+})
+
+app.post('/sign-in', (req, res) => {
+    console.log(req.body);
+    res.render('home');
+});
+
+app.post('/sign-up', (req, res) => {
+    console.log(req.body);
+    res.render('home');
+});
+
 
 // the server and port
 app.listen(8080, function () {
